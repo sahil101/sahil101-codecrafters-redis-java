4114bb6596f105ea167839adb8cb0c74de23bc50
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2025-03-27 17:21:24 +0530

    handle concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,43 +4,57 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+  private static final int PORT = 6379;
+  private static final int THREAD_POOL_SIZE = 10;
 
-    //  Uncomment this block to pass the first stage
-       ServerSocket serverSocket = null;
-       Socket clientSocket = null;
-       int port = 6379; // redis server default port
-       try {
-         serverSocket = new ServerSocket(port);
-         // Since the tester restarts your program quite often, setting SO_REUSEADDR
-         // ensures that we don't run into 'Address already in use' errors
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         while ((clientSocket = serverSocket.accept()) != null) {
-           BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-           OutputStream outputStream = clientSocket.getOutputStream();
-           String inputLine;
-           while ((inputLine = in.readLine()) != null) {
-             System.out.println(inputLine.toLowerCase());
-             if ("ping".equals(inputLine.toLowerCase())) {
-               outputStream.write("+PONG\r\n".getBytes());
-             }
-           }
-         }
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           if (clientSocket != null) {
-             clientSocket.close();
-           }
-         } catch (IOException e) {
-           System.out.println("IOException: " + e.getMessage());
-         }
-       }
+  public static void handleClient(Socket clientSocket) {
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      OutputStream outputStream = clientSocket.getOutputStream();
+      String inputLine;
+      while ((inputLine = in.readLine()) != null) {
+        System.out.println(inputLine.toLowerCase());
+        if ("ping".equals(inputLine.toLowerCase())) {
+          outputStream.write("+PONG\r\n".getBytes());
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+
+    ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+    System.out.println("Logs from your program will appear here!");
+    ServerSocket serverSocket = null; // server socket
+
+    int port = 6379; // redis server default port
+    try {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
+      while (true) {
+        Socket clientSocket = serverSocket.accept(); // client socket
+        System.out.println("Client connected: " + clientSocket.getInetAddress());
+        threadPool.execute(() -> handleClient(clientSocket));
+      }
+
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    }
   }
 }
